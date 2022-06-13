72b83a82f8071cdffe74394893b52d19d7fdffb7
xandelm <alexandrej.lima.mendes@gmail.com>
2022-06-13 17:07:34 -0300

    Cria classe Strategy e uma classe contexto Adicionar.

---
+++ src/main/java/com/mycompany/calculadorasimples/Strategy.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.calculadorasimples;
+
+/**
+ *
+ * @author Xande
+ */
+public interface Strategy {
+    public int operacao(int num1, int num2);
+}
